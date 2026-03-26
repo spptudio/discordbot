289d2bd6f1db7bd576eba1eafc473044b95d26fc
spptudio <77085824+spptudio@users.noreply.github.com>
2026-03-27 01:29:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+const { Client } = require('discord.js');
+
+// 🔥 서버 먼저
 require('http').createServer((req, res) => {
   res.end('ok');
 }).listen(process.env.PORT || 3000);
 
-const { Client } = require('discord.js');
-
 const client = new Client({ intents: [] });
 
-console.log('파일 실행됨');
+console.log("시작됨");
 
 client.once('ready', () => {
   console.log('READY SUCCESS');
 });
 
-console.log('로그인 시도');
-
 client.login(process.env.TOKEN)
   .then(() => console.log("LOGIN OK"))
   .catch(err => console.log("LOGIN ERROR:", err));
+
+// 🔥 프로세스 죽지 않게 유지
+setInterval(() => {}, 1000);
