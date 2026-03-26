ec3e257ebd8b05c245be60358693207159dcb553
spptudio <77085824+spptudio@users.noreply.github.com>
2026-03-27 01:43:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-console.log("start");
-const { Client } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-// 🔥 서버 먼저
-require('http').createServer((req, res) => {
-  res.end('ok');
-}).listen(process.env.PORT || 3000);
-
-const client = new Client({ intents: [] });
-
-console.log("시작됨");
-
-client.once('ready', () => {
-  console.log('READY SUCCESS');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.login(process.env.TOKEN)
-  .then(() => console.log("LOGIN OK"))
-  .catch(err => console.log("LOGIN ERROR:", err));
+client.once('ready', () => {
+  console.log('봇 실행됨');
+});
 
-// 🔥 프로세스 죽지 않게 유지
-setInterval(() => {}, 1000);
+const forumIds = [
+  '1485230539290972231',
+  '1245822867707396221',
+  '1245808001818955806',
+  '1282000814319075379',
+  '1245812636822802552',
+  '1245818716533293098',
+  '1245821548682674268'
+];
+
+const sentThreads = new Set();
+
+async function sendAlert(thread) {
+  try {
+    if (!thread.parent) await thread.fetch();
+    if (!thread.parent || !forumIds.includes(thread.parent.id)) return;
+
+    if (sentThreads.has(thread.id)) return;
+    sentThreads.add(thread.id);
+
+    const targetChannel = await client.channels.fetch('1486620867512500254');
+
+    const link = `https://discord.com/channels/${thread.guild.id}/${thread.id}`;
+
+    await targetChannel.send(`<@&1010225986748567684>\n${link}`);
+
+  } catch (err) {
+    console.log('에러:', err);
+  }
+}
+
+client.on('threadCreate', async (thread) => {
+  await sendAlert(thread);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (!message.channel.isThread()) return;
+
+  await sendAlert(message.channel);
+});
+
+client.login(process.env.TOKEN);
