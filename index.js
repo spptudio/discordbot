030477f89bc2cface920367ce6a8a79bae0c1270
spptudio <77085824+spptudio@users.noreply.github.com>
2026-03-27 00:56:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { Client } = require('discord.js');
-
-// 🔥 이거 꼭 있어야 함 (Render용)
 require('http').createServer((req, res) => {
   res.end('ok');
 }).listen(process.env.PORT || 3000);
 
+const { Client } = require('discord.js');
+
 const client = new Client({ intents: [] });
+
+console.log('파일 실행됨');
 
 client.once('ready', () => {
   console.log('READY SUCCESS');
